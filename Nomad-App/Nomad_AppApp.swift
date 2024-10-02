a7b291d29f75b77c1b5e5289f1eb5055ae2bfbed
Sébastien <sebbody79400@gmail.com>
2024-10-03 00:43:15 +0200

    Initial Commit

---
+++ Nomad-App/Nomad_AppApp.swift
@@ -0,0 +1,17 @@
+//
+//  Nomad_AppApp.swift
+//  Nomad-App
+//
+//  Created by Sebastien Besse on 03/10/2024.
+//
+
+import SwiftUI
+
+@main
+struct Nomad_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
